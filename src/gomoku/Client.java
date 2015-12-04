88ef382f421f82ebe6fdd704ff960f53ce7a1499
Octavianus Marcel Harjono <octavianusmarcel12@gmail.com>
2015-12-04 18:12:23 +0700

    Attemp to show room list

--- src/gomoku/Client.java
+++ src/gomoku/Client.java
@@ -166,6 +166,7 @@ public class Client {
                     playerid = msg.getUserid();
                     roomid = msg.getRoomid();
                     display("Logged in to room number " + roomid + ". Your player id is: " + playerid);
+                    gui.showRoomList(msg.getMessage());
                     break;
                 }                
                 case MessageToClient.MOVE:
